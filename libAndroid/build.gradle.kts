13776ce4294f05e199715c8d15406d06f88fd1b1
Etienne Caron <etienne.caron@gmail.com>
2022-05-23 14:06:19 -0400

    L03 rec: a) Add the Koin and Retrofit dependencies.

--- libAndroid/build.gradle.kts
+++ libAndroid/build.gradle.kts
@@ -35,10 +35,22 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
 
-    implementation("com.jakewharton.timber:timber:4.7.1")
+    // Simple DI
+    implementation("io.insert-koin:koin-android:3.2.0")
 
+    // Square Networking
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.0") // https://github.com/square/okhttp/blob/master/CHANGELOG.md
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0") // https://github.com/square/retrofit/blob/master/CHANGELOG.md
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Logging
+    implementation("com.jakewharton.timber:timber:4.7.1") // https://github.com/JakeWharton/timber/releases
+
+    // Unit tests
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.insert-koin:koin-test:3.2.0")
 
+    // Instrumentation & UI
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
