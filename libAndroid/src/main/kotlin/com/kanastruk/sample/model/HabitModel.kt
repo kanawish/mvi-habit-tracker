9cbac008a649c7f15549032e2f0ae68323af45f9
Etienne Caron <etienne.caron@gmail.com>
2022-06-06 17:04:54 -0400

    L04 rec: Safe state transitions, Intents and Side-effects

--- libAndroid/src/main/kotlin/com/kanastruk/sample/model/HabitModel.kt
+++ libAndroid/src/main/kotlin/com/kanastruk/sample/model/HabitModel.kt
@@ -40,23 +40,143 @@ import timber.log.Timber
  *
  */
 class HabitModel(
-    // TODO
+    authModel: AuthModel,
+    val habitApiBuilder:(authToken:String)->HabitApi,
+    private val scope: CoroutineScope
 ) {
-    // TODO: Add habitsStore StateFlow
-    // TODO: Add errors event SharedFlow
+    data class InternalState(
+        val credentials: Credentials,
+        val habitApi: HabitApi
+    )
 
+    private val _habitsStore = MutableStateFlow(HabitState(cacheState = LOADING))
+    val habitsStore = _habitsStore.asStateFlow()
+
+    // Errors are events, and not state, per say.
+    private val _errors = MutableSharedFlow<HabitError>()
+    val errors = _errors.asSharedFlow()
+
+    // We need a new Client/Api instance any time the authToken changes.
+    private val currentHabitApi = MutableStateFlow<InternalState?>(null)
+
+    private val apiFlowJob: Job
+
+    init {
+        val apiFlow = authModel.authStore
+            .map { authState ->
+                when(authState) {
+                    is AuthState.Ready -> authState.credentials
+                    else -> null
+                }
+            }
+            .map { nullableCreds -> nullableCreds?.let { creds ->
+                InternalState(creds, habitApiBuilder(creds.idToken))
+            } }
+
+        // We need to cancel this at 'end of life' for the Model.
+        apiFlowJob = apiFlow
+            .onEach { currentHabitApi.value = it }
+            .onCompletion { Timber.d("apiFlowJob completed $it") }
+            .launchIn(scope)
+
+        // When the api and credentials are available, execute the first reload call.
+        scope.launch {
+            currentHabitApi
+                .filterNotNull()
+                .first()
+                .let { (credentials,api) ->
+                    val userId = credentials.localId
+                    scope.launch {
+                        api.reloadProcess(userId)
+                    }
+                }
+        }
+    }
 
     /**
      * This is the simplest Intent processor.
      */
     private fun process(intent: Intent<HabitState>) {
-        TODO()
+        _habitsStore.value = intent.reduce(_habitsStore.value)
+    }
+
+    /**
+     * We're 'baby proofing' here, by soft failing if check fails.
+     *
+     * Another valid approach is to be 'strict' and throw exceptions at the slightest
+     * provocation. In that case, the expectation is the dev team catches and fixes issues
+     * before release.
+     *
+     * Baby proofing with a good error reporting framework is a more customer friendly
+     * approach IMO.
+     *
+     * @param predicate Check that current state is valid for the intent we want to process.
+     * @param intent The Intent function, exposes read-only InternalState.
+     */
+    private fun authedProcess(
+        predicate: (HabitState) -> Boolean,
+        intent: (InternalState, HabitState) -> HabitState
+    ) {
+        val currentHabitState = _habitsStore.value
+        currentHabitApi.value?.let { internalState ->
+            if (predicate(currentHabitState)) intent(internalState, currentHabitState)
+            else Timber.w("authedProcess() call ignored: predicate returned `false` for $currentHabitState.")
+        } ?: Timber.w("authedProcess() call ignored: internal state is null.")
+    }
+
+    // A good predicate example:
+    private fun isReloadable(state: HabitState):Boolean = when(state.cacheState) {
+        MODIFIED, FAILED, LOADED -> true
+        else -> false
+    }
+
+
+    // ***** PUBLIC PROCESS-INTENT FUNCTIONS *****
+
+    /**
+     * Process a 'reload' Intent, typically for pull-to-refresh
+     *
+     * Valid call from MODIFIED, FAILED, LOADED
+     */
+    fun processReloadIntent() {
+        authedProcess(::isReloadable) { (credentials, api),oldState->
+            val userId = credentials.localId
+            scope.launch(CoroutineName("HabitModel.processReloadIntent")) { api.reloadProcess(userId) }
+
+            oldState.copy(cacheState = LOADING).touch()
+        }
     }
 
     // TODO: Whole set of API intents (Load + CRuD of Habits and Entries)
     // NOTE: Keep those as mocks until we implement API calls.
+
+    // ***** PRIVATE RETROFIT/API DEPENDENT FUNCTIONS *****
+    /**
+     * Must be in 'LOADING' while waiting on callback.
+     */
+    private suspend fun HabitApi.reloadProcess(userId: String) {
+        val habits = getHabits(userId).body()
+        val entries = getEntries(userId).body()
+        if (habits != null && entries != null) {
+            process { old ->
+                old.copy(
+                    cacheState = LOADED,
+                    habits = habits,
+                    entries = entries
+                ).touch()
+            }
+        }
+    }
+
+    /**
+     * Call this as appropriate for your Lifecycle needs.
+     */
+    fun destroy() {
+        apiFlowJob.cancel()
+    }
+
 }
 
 /**
- * HabitApi builder function
+ * TODO HabitApi builder function
  */
