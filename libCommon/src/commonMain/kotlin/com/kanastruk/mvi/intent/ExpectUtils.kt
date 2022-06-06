52d9e3033db1664030bcce163c55678c125bc029
Etienne Caron <etienne.caron@gmail.com>
2022-06-06 16:35:54 -0400

    L03 rec: Adding `Intent` and `fun expectingIntent()` before Model creation.

---
+++ libCommon/src/commonMain/kotlin/com/kanastruk/mvi/intent/ExpectUtils.kt
@@ -0,0 +1,21 @@
+package com.kanastruk.mvi.intent
+
+import com.kanastruk.shared.logW
+
+/**
+ * E:Expected child, SC: Sealed class parent.
+ * When building a state machine using the sealed class approach, 'expecting()'
+ * allows you to create Intents that will log a warning and no-op when
+ * trying to execute an intent off of the wrong 'parent' (or 'old') state.
+ */
+inline fun <reified E : SC,SC> expectingIntent(crossinline block: (E) -> SC): Intent<SC> {
+   return Intent { old ->
+      when (old) {
+         is E -> block(old)
+         else -> {
+            logW("Expecting ${E::class.simpleName}, got ${old!!::class.simpleName}")
+            old
+         }
+      }
+   }
+}
